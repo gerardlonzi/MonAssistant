71ef60047a618c5a2c65e6d1890024ee86fb7c73
gerardlonzi <lonzigerard@gmail.com>
2025-05-30 22:01:38 +0100

    mise a jour de redis

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const redisCliecd nt = redis.createClient({
+const redisClient = redis.createClient({
     url: process.env.REDIS_URL,
 });
 

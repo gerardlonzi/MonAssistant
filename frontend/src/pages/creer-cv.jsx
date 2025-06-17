785b443b49794f6f54321b22aeacf4e967d746b7
gerardlonzi <lonzigerard@gmail.com>
2025-06-17 23:57:43 +0100

    amelioration du design de la select-cv page

--- frontend/src/pages/creer-cv.jsx
+++ frontend/src/pages/creer-cv.jsx
@@ -13,30 +13,30 @@ export default function Creer_Cv() {
     <>
    
       <Navbar user={user} IsWebsiteAdmin={IsWebsiteAdmin}/>
-    <Container className='mt-24 sm:mt-44'>
+    <Container className='mt-24 sm:mt-40'>
       <section className='mt-20 text-[#00275b]'>
         <h1 className='text-3xl font-semibold text-center mb-5 sm:mb-14 '>Créez un <span className='text-green-600'>CV gagnant</span>  en quelques minutes</h1>
-        <div className='flex flex-col sm:flex-row items-center justify-center gap-5 sm:gap-16 mb-6 sm:mb-16 text-center relative'>
+        <div className='flex flex-col sm:flex-row items-center justify-center gap-5 sm:gap-16 mb-3 sm:mb-10 text-center relative'>
           <div className='flex flex-col items-center justify-center'>
             <div className='flex items-center relative '>
               <img className='p-5 bg-[#f0fff5] rounded-lg w-24 sm:w-full' src="/svgs/rbd-hiw-template-icon.svg" alt="cHOISIR_UN_MODELES" />
               <span className='sm:w-10 sm:h-10 h-7 w-7 -left-[1rem] sm:-left-5 bg-[#00275b] absolute -left-5 text-white flex items-center justify-center rounded-full font-bold'>1</span>
             </div>
-            <p className='text-xl mt-4 sm:mt-10'>Choisissez votre modèle conçu par des professionnels</p>
+            <p className='text-xl mt-4 sm:mt-10'>Choisissez votre modèle conçu <br/> par des professionnels</p>
           </div>
           <div className='flex flex-col items-center justify-center'>
             <div className='flex items-center relative '>
               <img className='p-5 bg-[#f0fff5] rounded-lg w-24 sm:w-full' src="/svgs/rbd-hiw-example-icon.svg" alt="AJOUTER_UN_MODELE_PRECIS" />
               <span className='sm:w-10 sm:h-10 h-7 w-7 -left-[1rem] sm:-left-5 bg-[#00275b] absolute  text-white flex items-center justify-center rounded-full font-bold'>2</span>
             </div>
-            <p className='text-xl mt-4 sm:mt-10'>Ajoutez des exemples pré-écrits à chaque section</p>
+            <p className='text-xl mt-4 sm:mt-10'>Ajoutez des exemples pré-écrits <br/> à chaque section</p>
           </div>
           <div className='flex flex-col items-center justify-center'>
             <div className='flex items-center relative '>
               <img className='p-5 bg-[#f0fff5] rounded-lg w-24 sm:w-full' src="/svgs/rbd-hiw-download-icon.svg" alt="telecharger_et_postuler_icon" />
               <span className='sm:w-10 sm:h-10 h-7 w-7 -left-[1rem] sm:-left-5 bg-[#00275b] absolute  text-white flex items-center justify-center rounded-full font-bold'>3</span>
             </div>
-            <p className='text-xl mt-4 sm:mt-10'>Téléchargez et commencez à postuler</p>
+            <p className='text-xl mt-4 sm:mt-10'>Téléchargez et commencez <br/> à postuler</p>
           </div>
           <div className='text-xl text-black rounded-full flex flex-col sm:flex-row hidden sm:flex gap-3 absolute -z-10 top-[67px]'>
             <span>•</span>
@@ -92,7 +92,7 @@ export default function Creer_Cv() {
           </div>
         </div>
         <div className='flex justify-center'> 
-          <LinkComponent className={"px-[5rem]"} to={"/creer-cv/niveau-de-connaissances"} variant={"tercero"} content={"commencer"}/>
+          <LinkComponent className={"px-[5rem] "} to={"/creer-cv/niveau-de-connaissances"} variant={"tercero"} content={"commencer "}/>
         </div>
       </section>
 
--- frontend/src/pages/select-cv.jsx
+++ frontend/src/pages/select-cv.jsx
@@ -16,17 +16,21 @@ export default function Select_cv() {
       <Navbar user={user} IsWebsiteAdmin={IsWebsiteAdmin} />
     <Container className='mt-36'>
             <div>
-                <h1 className='sm:text-4xl text-3xl font-bold text-center text-secondary'>Comment voulez vous créer votre cv</h1>
+                <h1 className='sm:text-4xl text-3xl font-bold text-center  text-secondary'>Comment voulez vous créer votre cv</h1>
                 <div className='flex flex-col sm:flex-row justify-center items-center mt-7 sm:mt-16 gap-5'>
-                <div className='outline outline-gray-300 rounded-md hover:outline-3 hover:outline-black transition-all w-full sm:w-[35rem] px-10 py-14'>
-                    <div></div>
+                <div className='outline outline-gray-300 rounded-md hover:outline-3 hover:outline-black transition-all w-full sm:w-[35rem] px-10 py-8 sm:py-14 text-center sm:text-left flex gap-5 flex-col sm:flex-row justify-center items-center'>
+                    <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-plus-icon lucide-square-plus"><rect width="18" height="18" x="3" y="3" rx="2" /><path color="#00cf5c" d="M8 12h8"/><path d="M12 8v8" color="#00cf5c"/></svg>
+                    </div>
                     <div>
                         <div className='text-lg sm:text-xl font-bold'>Créer un nouveau CV</div>
                         <div className='text-sm mt-[1px]'>Nous aidons a créer un cv étape par étape</div>
                     </div>
                 </div>
-                <div className='outline outline-gray-300 rounded-md hover:outline-3 hover:outline-black transition-all w-full sm:w-[35rem] px-10 py-14'>
-                    <div></div>
+                <div className='outline text-center sm:text-left outline-gray-300 rounded-md hover:outline-3 hover:outline-black transition-all w-full sm:w-[35rem] px-10 py-8 sm:py-14 flex gap-5 flex-col sm:flex-row justify-center items-center'>
+                    <div >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1"  stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-upload-icon lucide-upload"><path color="blue" d="M12 3v12"/><path d="m17 8-5-5-5 5" color="blue"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /></svg>
+                    </div>
                     <div>
                         <div className='text-lg sm:text-xl font-bold'>J'ai déjà un CV</div>
                         <div className='text-sm mt-[1px]'>Nous reformatons votre cv et remplissons les information </div>
